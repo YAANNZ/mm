19f3dcd527d632737beea5bfc8b92aa4690fa7db
zyn <zhuyanan@powerbeijing.com>
2021-10-29 16:23:28 +0800

    router

---
+++ zzreader/lib/core/router/router.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ZZRouter {
+  // static final String initialRouter = ;
+
+  static final Map<String, Widget> routers = {
+
+  };
+
+  static final RouteFactory generateRoute = (settings) {
+    return null;
+  };
+
+  static final RouteFactory unknownRoute = (settings) {
+    return null;
+  };
+}
