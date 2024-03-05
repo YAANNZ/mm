17f4c5f9e9e932fc8b5314f851b58d80869aa937
Zhu Yanan（Lofty Team） <yanan.zhu@lofty.com>
2024-03-05 17:38:38 +0800

    feat: add record collectionView

---
+++ mm/mm/Score/ZMMScoreRecordView.swift
@@ -0,0 +1,38 @@
+//
+//  ZMMScoreRecordView.swift
+//  mm
+//
+//  Created by Tommy Liu（Loftyworks Team） on 2024/3/1.
+//
+
+import UIKit
+
+class ZMMScoreRecordView: UIView {
+
+    var scrollView: UIScrollView?
+    
+//    RRTabMoreConfig *config = [RRTabMoreConfig shareInstance];
+//    config.sectionInset = UIEdgeInsetsMake(30, 20, 30, 20);
+//    config.tabItemWidth = floorf((SCREEN_WIDTH - 40) / 5);
+//    
+//    UICollectionViewFlowLayout *flowlayout = [[UICollectionViewFlowLayout alloc] init];
+//    flowlayout.minimumLineSpacing = 20;
+//    flowlayout.sectionInset = config.sectionInset;
+//    CGFloat itemHeight = config.imageOffset+config.tabImageSize.height+config.titleOffset+config.titleHeight;
+//    flowlayout.itemSize = CGSizeMake(config.tabItemWidth, itemHeight);
+//
+////        RRMaxCellSpacingFlowLayout *spacelayout = [[RRMaxCellSpacingFlowLayout alloc] init];
+////        spacelayout.maxCellSpacing = config.tabItemSpacing;
+////        spacelayout.minimumLineSpacing = config.tabItemLineSpacing;
+////        spacelayout.sectionInset = config.sectionInset;
+////        CGFloat itemHeight = config.imageOffset+config.tabImageSize.height+config.titleOffset+config.titleHeight;
+////        spacelayout.itemSize = CGSizeMake(config.tabItemWidth, itemHeight);
+//
+//    _tabItemsClView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flowlayout];
+//    _tabItemsClView.backgroundColor = [UIColor clearColor];
+//    _tabItemsClView.delegate = self;
+//    _tabItemsClView.dataSource = self;
+//    [_tabItemsClView registerClass:[RRTabMoreClVCell class] forCellWithReuseIdentifier:NSStringFromClass(RRTabMoreClVCell.class)];
+    
+
+}
--- mm/mm/Score/ZMMScoreVC.swift
+++ mm/mm/Score/ZMMScoreVC.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import Masonry
 
 class ZMMScoreVC: UIViewController {
 
     @IBOutlet weak var contentBgView: UIView!
-    
-    
+    @IBOutlet weak var recordBgView: UIView!
+    @IBOutlet weak var optionsBgView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,18 @@ class ZMMScoreVC: UIViewController {
 
     func configUI() {
 //        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: <#T##UIImage?#>, style: <#T##UIBarButtonItem.Style#>, target: <#T##Any?#>, action: <#T##Selector?#>)
+        
+        let recordView = ZMMScoreRecordView()
+        recordBgView.addSubview(recordView)
+        recordView.mas_makeConstraints { make in
+            make?.edges.equalTo()
+        }
+        
+        let optionsView = ZMMScoreOptionsView()
+        optionsBgView.addSubview(optionsView)
+        optionsView.mas_makeConstraints { make in
+            make?.edges.equalTo()
+        }
     }
 
     
