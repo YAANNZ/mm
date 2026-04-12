ea8fedbec325e77d388daa3ee134088da5cd01b8
zhuyn <zhuynchn@163.com>
2026-04-12 22:50:03 +0800

    fix: 解决 Flutter 首次安装后局域网请求被拒绝的问题
    Dart VM 的 HTTP 请求走 BSD socket 不经过 NSURLSession，
    首次连接局域网 IP 时不会触发 iOS 网络权限弹窗。
    Debug 模式下在 AppDelegate 启动时发一个 NSURLSession 请求强制触发弹窗。
    同时补全 Info.plist 的 ATS 和本地网络权限声明。

--- mm/mm/Main/AppDelegate.m
+++ mm/mm/Main/AppDelegate.m
@@ -16,6 +16,15 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+#ifdef DEBUG
+    // Flutter (Dart VM) 的 HTTP 请求走 BSD socket 而非 NSURLSession，
+    // 首次安装后直连局域网 IP 不会触发 iOS 网络权限弹窗，导致请求直接失败 (errno 65)。
+    // 这里用 NSURLSession 发一个轻量请求，强制 iOS 弹出网络权限对话框。
+    // 授权后 Dart 的 socket 请求也能正常访问局域网。仅 Debug 模式执行。
+    [[NSURLSession.sharedSession dataTaskWithURL:[NSURL URLWithString:@"https://www.apple.com"]
+                               completionHandler:^(NSData *d, NSURLResponse *r, NSError *e) {}] resume];
+#endif
+
     // Pre-warm Flutter engine
     self.flutterEngine = [[FlutterEngine alloc] initWithName:@"main_engine"];
     [self.flutterEngine run];
